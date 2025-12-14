1f773c3f2fa5128f208601f88c39d2a6e295460f
Allen Alex <allenalex17345@gmail.com>
2025-12-14 21:47:44 +0000

    Add init.lua and lua directory

---
+++ init.lua
@@ -0,0 +1,23 @@
+-- #  ================================================== #
+
+-- # Neovim config by allen-alex
+
+-- #  ================================================== #
+
+
+-- [Require Config files] --
+
+require("config.mappings")
+require("config.options")
+require("config.autocmd")
+
+
+-- [Plugins]
+
+require("config.lazy")
+
+
+-- Treesitter Updates
+require("lazy").setup({
+  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
+})
