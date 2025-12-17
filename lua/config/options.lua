687aa69af5c42dac5925a0beb976d7991af2d8f5
Allen Alex <allenalex17345@gmail.com>
2025-12-17 21:58:54 +0000

    Add spellcheck and spelllang

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,19 +1,21 @@
 local options = {
 	
 	termguicolors = true,
-
 	undofile = true,
 	cursorline = true, --highlight line
-
 	wrap = true,
 	ttyfast = true,
 	smoothscroll = true,
-	
 	title = true, --automatic window titlebar
 	clipboard = "unnamedplus",
 	history = 25,
+	swapfile = false,
 
-
+	number = true, 
+	relativenumber = false,
+	numberwidth = 4,
+	spelllang = "en_gb",
+	spell = true,
 	-- [Indentation stuff] --
 	smarttab = true,
 	cindent = true,
@@ -22,10 +24,7 @@ local options = {
 	softtabstop = 4,
 	shiftwidth = 4,
 	-- ################### --
-	
-	number = true, 
-	relativenumber = false,
-	numberwidth = 4,
+
 }
 
 for k, v in pairs(options) do
