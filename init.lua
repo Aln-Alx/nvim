e322891025bdbe0a378de6a3b4e34ab1a56f2167
Allen Alex <allenalex17345@gmail.com>
2025-12-17 16:51:01 +0000

    Add theme

--- init.lua
+++ init.lua
@@ -7,15 +7,13 @@
 
 -- [Require Config files] --
 
+require("config.lazy")
 --require("config.mappings")
 --require("config.options")
 --require("config.autocmd")
-
+require("config.theme")
 
 -- [Plugins]
-
-require("config.lazy")
-
 
 -- Treesitter Updates
 require("lazy").setup({
