3007727a5e54e4c5867e508dbe45634f301a8102
Allen Alex <allenalex17345@gmail.com>
2025-12-17 16:17:59 +0000

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,10 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate", -- Automatically update parsers on plugin update
-        lazy = false,
-
-        config = function() 
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-				-- Install parsers synchronously (only applied to `ensure_installed`)                
-				sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-            })
-        end
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate", -- Automatically update parsers on plugin update
+    lazy = false,
+    opts = {
+      ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline",},
     },
+  },
 }
