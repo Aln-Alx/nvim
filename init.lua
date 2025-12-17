4942c6c873e64eb0a1a2fae8057d137b43e65c1d
Allen Alex <allenalex17345@gmail.com>
2025-12-17 19:59:30 +0000

    Fix re-sourcing config error
    Removed require lazy in init.lua and added in treesitter config file.

--- init.lua
+++ init.lua
@@ -9,13 +9,7 @@
 
 require("config.lazy")
 --require("config.mappings")
---require("config.options")
+require("config.options")
 --require("config.autocmd")
 require("config.theme")
 
--- [Plugins]
-
--- Treesitter Updates
-require("lazy").setup({
-  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
-})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,8 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
+	build = ":TSUpdate", -- Automatically update parsers on plugin update
+	branch = 'master',
     opts = {
       ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline",},
     },
