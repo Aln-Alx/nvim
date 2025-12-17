b3ac96628f14638a15f60d6567f99a104c3a8f8a
Allen Alex <allenalex17345@gmail.com>
2025-12-17 17:17:45 +0000

    Add options.lua

---
+++ lua/config/options.lua
@@ -0,0 +1,33 @@
+local options = {
+
+	undofile = true,
+	cursorline = true, --highlight line
+
+	wrap = true,
+	ttyfast = true,
+	smoothscroll = true,
+	
+	title = true, --automatic window titlebar
+	clipboard = "unnamedplus",
+	history = 25,
+
+
+	-- [Indentation stuff] --
+	smarttab = true,
+	cindent = true,
+	autoindent = true,
+	tabstop = 4, 
+	softtabstop = 4,
+	shiftwidth = 4,
+	-- ################### --
+	
+	number = true, 
+	relativenumber = false,
+	numberwidth = 4,
+}
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+
+
