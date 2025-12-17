a92c90f294a86ad0cb25c2fca624162b2ad0f342
Allen Alex <allenalex17345@gmail.com>
2025-12-17 15:31:08 +0000

    Fix indentation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,8 +7,8 @@ return {
         config = function() 
             require("nvim-treesitter.configs").setup({
                 ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-		-- Install parsers synchronously (only applied to `ensure_installed`)                
-		sync_install = false,
+				-- Install parsers synchronously (only applied to `ensure_installed`)                
+				sync_install = false,
                 auto_install = true,
                 highlight = {
                     enable = true,
