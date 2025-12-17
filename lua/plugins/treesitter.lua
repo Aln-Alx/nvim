c80584f17f6d9b3ad76a493b988b7956f346bd93
Allen Alex <allenalex17345@gmail.com>
2025-12-17 16:31:54 +0000

    Remove redundant build statement

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate", -- Automatically update parsers on plugin update
     lazy = false,
     opts = {
       ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline",},
