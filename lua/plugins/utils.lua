a738900b3ecf5c3e0c41e7d1b95df2dca03e20ba
Allen Alex <allenalex17345@gmail.com>
2025-12-18 22:34:51 +0000

    Add lualine status line plugin

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -2,5 +2,10 @@ return {
 	{"nvim-tree/nvim-web-devicons"},
 	{"lewis6991/gitsigns.nvim"},
 	{"folke/which-key.nvim"},
-	{"rcarriga/nvim-notify"}
+	{"rcarriga/nvim-notify"},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { 'nvim-tree/nvim-web-devicons'},
+		opts = {}
+	}
 }
