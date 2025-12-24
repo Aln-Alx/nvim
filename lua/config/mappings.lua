46aad0de0df9998a05bd79185f6adb7b918bdade
Allen Alex <allenalex17345@gmail.com>
2025-12-24 21:15:34 +0000

    Add oil.nvim launch mapping

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,7 +1,14 @@
 vim.g.mapleader = " "
 
--- Set Keymaps --
+-- [Set Keymaps] --
 
+
+-- Buffer Operations
 vim.keymap.set("n", "<Leader>bn", ":BufferNext<CR>", {noremap = true, silent = true})
 vim.keymap.set("n", "<Leader>bp", ":BufferPrevious<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<Leader>bd", ":BufferDelete<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<Leader>bc", ":tabnew<CR>", {noremap = true, silent = true})
 
+
+-- Plugins
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
