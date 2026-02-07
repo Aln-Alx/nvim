14a4c0eea80e4502a3decc0a838da40be879ba0f
Allen Alex <allenalex17345@gmail.com>
2026-02-07 17:13:56 +0000

    fix: move treesitter to main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
 	build = ":TSUpdate", -- Automatically update parsers on plugin update
-	branch = 'master',
+	branch = "main",
     opts = {
       ensure_installed = {"c", "lua", "rust", "zig", "vim", "vimdoc", "query", "markdown", "markdown_inline",},
 	  highlight = {enable = true},
