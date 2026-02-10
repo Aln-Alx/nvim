6b0cfa620bf3c72c3b75e857e4bff0a91478ed41
Allen Alex <allenalex17345@gmail.com>
2026-02-10 20:49:27 +0000

    refactor: remove unnecessary comments

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -30,19 +30,6 @@ return {
     --- @type blink.cmp.Config
     opts = {
       keymap = {
-        -- "default" (recommended) for mappings similar to built-in completions
-        --   <c-y> to accept ([y]es) the completion.
-        --    This will auto-import if your LSP supports it.
-        --    This will expand snippets if the LSP sent a snippet.
-        -- "super-tab" for tab to accept
-        -- "enter" for enter to accept
-        -- "none" for no mappings
-        --
-        -- For an understanding of why the "default" preset is recommended,
-        -- you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        --
         -- All presets have the following mappings:
         -- <tab>/<s-tab>: move to right/left of your snippet expansion
         -- <c-space>: Open menu or open docs if already open
@@ -58,8 +45,6 @@ return {
       },
 
       appearance = {
-        -- "mono" (default) for "Nerd Font Mono" or "normal" for "Nerd Font"
-        -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = "mono",
       },
 
